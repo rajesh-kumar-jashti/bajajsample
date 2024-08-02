d8de8708fa08a0512083f7ef29ed3db0e4a3bea1
JashtiGopinath <rajeshkumarjashti@gmail.com>
2024-08-02 21:04:23 +0530

    cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
-const cors = require('cors')
 
-app.use(cors);
+app.use(cors())
 app.use(express.json());
 
 app.post('/bfhl', (req, res) => {
